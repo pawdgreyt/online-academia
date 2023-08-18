ad4aae6b0919720e5de0faf0cda241cc05c9e115
chanz-pclimaco <pclimaco@chanzglobal.com>
2023-08-18 14:35:18 +0800

    Student Information UI

--- resources/js/Pages/Student.vue
+++ resources/js/Pages/Student.vue
@@ -7,10 +7,9 @@ defineProps({ student: Array });
 <template>
     <Head title="Student Details" />
     <NavLayout>
-        <span class="text-white">{{ student }}</span>
         <main class="container mx-auto py-8">
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-                <div class="md:col-span-3 bg-gray-800 rounded-lg p-6 flex">
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div class="md:col-span-4 bg-gray-800 rounded-lg p-6 flex">
                     <img
                         class="rounded-full"
                         width="35"
@@ -20,12 +19,31 @@ defineProps({ student: Array });
                         Details of Student: {{ student.name }}
                     </h2>
                 </div>
-                <div class="md:col-span-3 bg-gray-800 rounded-lg p-6 flex">
-                    <h4 class="text-white ml-2 font-extrabold py-1">
-                        Student Information
-                    </h4>
+                <div class="md:col-span-2 bg-gray-800 rounded-lg p-6 flex">
+                    <div class="mr-4">
+                        <p class="text-white font-semibold">Name:</p>
+                        <p class="text-white">{{ student.name }}</p>
+
+                        <p class="text-white font-semibold mt-3">Email:</p>
+                        <p class="text-white">{{ student.email }}</p>
+
+                        <p class="text-white font-semibold mt-3">
+                            Account Created:
+                        </p>
+                        <p class="text-white">{{ student.created_at }}</p>
+                    </div>
                 </div>
-                <div class="md:col-span-3 bg-gray-800 rounded-lg p-6 flex">
+                <div class="md:col-span-2 bg-gray-800 rounded-lg p-6 flex">
+                    <div class="mr-4">
+                        <p class="text-white font-semibold">Account ID:</p>
+                        <p class="text-white">{{ student.id }}</p>
+                        <p class="text-white font-semibold mt-3">Role:</p>
+                        <p class="text-white">{{ student.role }}</p>
+                        <p class="text-white font-semibold mt-3">Auth Type:</p>
+                        <p class="text-white">{{ student.auth_type }}</p>
+                    </div>
+                </div>
+                <div class="md:col-span-4 bg-gray-800 rounded-lg p-6 flex">
                     <h4 class="text-white ml-2 font-extrabold py-1">
                         Courses Enrolled
                     </h4>
